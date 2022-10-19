afde89bbe899146b45b44a42198e9716ed2f8f0e
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2022-10-19 08:48:57 -0300

    atualizar

---
+++ processa.php
@@ -0,0 +1,26 @@
+<?php
+
+//Recebe os campos do formulário
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$sexo = $_POST['sexo'];
+$idade = $_POST['idade'];
+$telefone = $_POST['telefone'];
+$senha = $_POST['senha'];
+$estado = $_POST['estado'];
+$cidade = $_POST['cidade'];
+$destinos = $_POST['destinos'];
+$hospedagem = $_POST['hospedagem'];
+$mensagem = $_POST['mensagem'];
+$dt_cadastro = $_POST['dt-cadastro'];
+
+
+
+
+//para investigar variaveis e expressões
+//var_dump($_POST);
+
+
+
+
+
