4d06c4d9be2b2000f256e2f9396b53ea0a823e0a
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2022-10-19 10:56:21 -0300

    atualizar php

--- processa.php
+++ processa.php
@@ -15,11 +15,22 @@ $mensagem = $_POST['mensagem'];
 $dt_cadastro = $_POST['dt-cadastro'];
 
 
-
-
 //para investigar variaveis e expressões
 //var_dump($_POST);
 
+
+//CONECTA AO BANCO E GRAVA OS DADOS (INSERT COM PDO)
+try {
+    //code...
+    $pdo = new PDO('mysql:host=localhost;dbname=explore', 'root', '');
+    //INSERT na tabela users
+    $sql = $pdo->prepare('INSERT into ursers(nome)')
+
+} catch (PDOException $erro) {
+    //throw $th;
+    //se ter erro exibe o erro
+    echo $erro;
+}
 
 
 
